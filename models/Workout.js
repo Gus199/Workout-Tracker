977b5e0174d502f8b6beff67c7ca1c0b4995d0ef
Amar <augustine2903a@gmail.com>
2022-01-20 19:25:00 -0500

    updates server.js

--- models/Workout.js
+++ models/Workout.js
@@ -1,44 +1,72 @@
+// Workout Model - all models exported by index.js
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now
+        default: Date.now(),
     },
-    exrcises: [{
-        type: {
-            type: String,
-            time: true,
-            require: "Workout type is required"
-        },
-        name: {
-            type: String,
-            trim: true,
-            require: "Write name of Workout"
-        },
-        duration: {
-            type: Number,
-            required: "Enter duration in minutes"
-        },
-        wieght: {
-            type: Number,
-        },
-        reps: {
-            type: Number,
-
-        },
-        sets: {
-            type: Number,
-        },
-        distance: {
-            type: Number,
-
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                required: true
+            },
+            weight: {
+                type: Number,
+                trim: true,
+                required: true
+            },
+            reps: {
+                type: Number,
+                trim: true,
+                required: true
+            },
+            sets: {
+                type: Number,
+                trim: true,
+                required: true
+            },
+            distance: {
+                type: Number,
+                trim: true,
+                required: true
+            }
         }
-    }]
-})
+    ]
+}, {
+    toObject: {
+    virtuals: true
+    },
+    toJSON: {
+    virtuals: true 
+    }
+  });
+ 
+WorkoutSchema
+.virtual('totalDuration')
+.get(function () {
+    const exercises = this.exercises;
+    let total = 0;
+    for (let i = 0; i < exercises.length; i++) {
+        total += exercises[i].duration;
+    }
+    return total;
+    
+});
 
-const Workout = mongoose.model("Workout", workoutSchema)
+const Workout = mongoose.model('Workout', WorkoutSchema);
 
-module.exports = Workout
+module.exports = Workout;
